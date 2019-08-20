dfe98389deb86976a579e0c761544ea7ecaf39ed
Zhenguo Zhang <zhangz.sci@gmail.com>
2019-08-20 00:43:02 +0000

    add more functions to bash_functions.sh

--- bash_functions.sh
+++ bash_functions.sh
@@ -7,8 +7,29 @@ function msg()
 	echo -e "$*" >&2
 }
 
+function error()
+{
+	msg "[ERROR] $*"
+}
 
-# testing
+function warn()
+{
+	msg "[WARN] $*"
+}
+
+## get the number of fields in a file
+function file_nf
+{
+    f=$1;
+    sp=${2:-"\t"}
+    nf=($(less $f | gawk -v FS=$sp 'NR<100{print NF}' | uniq))
+    if [[ ${#nf[@]} -gt 1 ]]; then
+            echo "Inconsistent number of fields in $f"
+    else
+            echo $nf
+    fi
+}
+
 ## check whether a command is available
 function check_exe
 {
@@ -18,6 +39,42 @@ function check_exe
 		echo "";
 	fi
 }
+
+# string functions
+## split a string to separate lines
+function str_split
+{
+	sp=$1;
+	st=$2;
+	echo "$st" | sed -e "s/$sp/\n/g"
+}
+
+## generate random alphabet string
+function rand_str
+{
+	len=${1:-8}; # string length
+	echo $(head -10 /dev/urandom | tr -dc '0-9a-zA-Z' | fold -w $len | head -1)
+}
+
+## test whether a string is number
+function is_num
+{
+	if [[ "$1" =~ ^-?[0-9.]+$ ]]; then
+		echo "ok"
+	else
+		echo ""
+	fi
+}
+
+function is_int
+{
+	if [[ "$1" =~ ^-?[0-9]+$ ]]; then
+		echo "ok"
+	else
+		echo "";
+	fi
+}
+
 
 # computing
 ## use bc to compare/compute numbers
